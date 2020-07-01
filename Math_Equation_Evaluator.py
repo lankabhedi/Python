678006a26e8c097118aeb19933c0cf37a3ed50fa
lankabhedi <50140975+lankabhedi@users.noreply.github.com>
2020-07-01 07:29:49 +0530

    Update Math_Equation_Evaluator.py

--- Math_Equation_Evaluator.py
+++ Math_Equation_Evaluator.py
@@ -6,7 +6,7 @@ print("""
 *---------Made By: Samnit Mehandiratta---------*
 ************************************************
 
-Enter 'q' to quit and 'h' for help.
+Enter 'q' to quit, 'r' to reset and 'h' for help.
 Hint: Type your equation after the prompt '>' and press Enter.
 """)
 
@@ -27,6 +27,10 @@ def calculator():
     if equation == 'q':
         print("See you later human-like creature!")
         switch = False
+    elif equation == 'r':
+        answer = ""
+    elif equation == "":
+        pass
     elif equation == 'h':
         print("""
         The calculator supports addition(+), subtraction(-), multiplication(*), exponent(**),
@@ -35,11 +39,26 @@ def calculator():
     # elif re.sub('[1234567890+*/" "]', '', equation) == "" or re.sub('[1234567890]', '', equation) == "-":
     # print("Please enter a number after the operator.")
     else:
-        equation = re.sub('[A-Za-z;:)(&^%$#@!|\'<>.,?"]', '', equation)
-        equation = equation.replace(" ", "")
-        equation2 = str(answer) + equation
-        answer = eval(equation2)
-        # print("Solution = " + answer)
+        if "/0" in equation:
+            print("Infinity")
+            answer = ""
+        else:
+            equation = "@" + equation
+            if (len(((re.sub('[A-Za-z;:)(&^%$#!|\'<>.,?="]', '', equation)).rstrip())) > 1) or (
+                    ((re.sub('[A-Za-z;:)(&^%$#!|\'<>.,?="]', '', equation)).rstrip())[-1] in ["+", "-", "/", "*"]):
+                equation = ((re.sub('[A-Za-z;:)(&^%$#@!|\'<>.,?="]', '', equation)).rstrip()).rstrip("*")
+                equation = ((re.sub('[A-Za-z;:)(&^%$#@!|\'<>.,?="]', '', equation)).rstrip()).rstrip("+")
+                equation = ((re.sub('[A-Za-z;:)(&^%$#@!|\'<>.,?="]', '', equation)).rstrip()).rstrip('/')
+                equation = ((re.sub('[A-Za-z;:)(&^%$#@!|\'<>.,?="]', '', equation)).rstrip()).rstrip("-")
+                equation2 = str(answer) + equation
+                answer = eval(equation2)+0
+
+            else:
+                equation = re.sub('[A-Za-z;:)(&^%$#@!|\'<=>.,?"]', '', equation)
+                equation = equation.replace(" ", "")
+                equation2 = str(answer) + equation
+                answer = eval(equation2)+0
+                # print("Solution = " + answer)
 
 
 while switch:
